e105c8119b18eab5522c6d77be7a635d83259255
Maroof Shittu <dbugshe2@gmail.com>
2018-05-24 21:44:43 +0100

    update server.py to display info about the request

--- server.py
+++ server.py
@@ -30,7 +30,15 @@ class RequestHandler(BaseHTTPRequestHandler):
     def create_page(self):
         '''This is where we fill in the formatting placeholders
         '''
-        
+        values = {
+            'date_time': self.date_time_string(),
+            'client_host': self.client_address[0],
+            'client_port': self.client_address[1],
+            'command': self.command,
+            'path': self.path
+        }
+        page = self.Page.format(**values)
+        return page
 
     def send_page(self, page):
         self.send_response(200)
