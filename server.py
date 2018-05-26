eb49f93af987757932f8c2ff12025fd1f2e4ac81
Maroof Shittu <dbugshe2@gmail.com>
2018-05-26 18:32:46 +0100

    debug handle_error to display error page

--- server.py
+++ server.py
@@ -2,24 +2,13 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
 
 
+class ServerException(Exception):
+    '''For internal error reporting'''
+    pass
+
+
 class RequestHandler(BaseHTTPRequestHandler):
     '''handle HTTP request by returning a fixed 'page'. '''
-
-    # the page is a string containig some html with formatting placeholders
-    Page = '''\
-<html>
-<body>
-<table>
-<tr>  <td>Header</td>         <td>Value</td>          </tr>
-<tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
-<tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
-<tr>  <td>Client port</td>    <td>{client_port}s</td> </tr>
-<tr>  <td>Command</td>        <td>{command}</td>      </tr>
-<tr>  <td>Path</td>           <td>{path}</td>         </tr>
-</table>
-</body>
-</html>
-'''
 
     Error_Page = """\
     <html>
@@ -32,15 +21,14 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     # Handle a GET request
     def do_GET(self):  # This method is called when the HTTP method is GET
-        '''Overide of the do_GET() method in BaseRequestHandler
-        '''
+        '''Overide of the do_GET() method in BaseRequestHandler'''
         try:
             # figure out exactly what is requested
             full_path = os.getcwd() + self.path
 
             # it does does not exist....
             if not os.path.exists(full_path):
-                ServerException("{} not found".format(self.path))
+                raise ServerException("file at{0} not found".format(self.path))
 
             # ...it's a file...
             elif os.path.isfile(full_path):
@@ -48,46 +36,39 @@ class RequestHandler(BaseHTTPRequestHandler):
 
             # ...it's something we don't handle
             else:
-                raise ServerException("Unknown Object {}".format(self.path))
+                raise ServerException("Unknown Object {0}".format(self.path))
         # handle errors
         except Exception as msg:
-            self.handle_errors(msg)
+            self.handle_error(msg)
 
-    def handle_file(self):
+    def handle_file(self, full_path):
         try:
             with open(full_path, 'rb') as reader:
                 content = reader.read()
             self.send_content(content)
         except IOError as msg:
-            msg = "'{0}' cannot bee read: {1}".format(self.path, msg)
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
 
+    # handle unknown objects
     def handle_error(self, msg):
         content = self.Error_Page.format(path=self.path, msg=msg)
-        self.send_content(content)
+        self.send_content(bytes(content, "utf-8"), 404)
 
-    def create_page(self):
-        '''This is where we fill in the formatting placeholders
-        '''
-        values = {
-            'date_time': self.date_time_string(),
-            'client_host': self.client_address[0],
-            'client_port': self.client_address[1],
-            'command': self.command,
-            'path': self.path
-        }
-        page = self.Page.format(**values)
-        return page
-
-    def send_page(self, page):
-        self.send_response(200)
+    def send_content(self, content, status=200):
+        self.send_response(status)
         self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(page)))
+        self.send_header("Content-Length", str(len(content)))
         self.end_headers()
-        self.wfile.write(bytes(page, "utf-8"))
+        self.wfile.write(content)
 
 
 if __name__ == '__main__':
-    serverAddress = ('', 8080)
-    server = HTTPServer(serverAddress, RequestHandler)
-    print("Server Running on 8080")
-    server.serve_forever()
+    try:
+        serverAddress = ('', 8080)
+        server = HTTPServer(serverAddress, RequestHandler)
+        print("Server Running on 8080")
+        server.serve_forever()
+    except (KeyboardInterrupt, Exception):
+        print("Closing server")
+        server.socket.close()
